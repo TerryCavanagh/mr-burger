e77043b50d844ac5de5388c9a0fdf4a5d058b0fc
TerryCavanagh <terrygcavanagh@gmail.com>
2023-04-29 16:03:00 +0100

    little playable area

--- scripts/platformer_player.gd
+++ scripts/platformer_player.gd
@@ -1,51 +1,61 @@
 extends CharacterBody2D
 
 @onready var Sprite = $AnimatedSprite2D;
+@onready var camera:Camera2D = get_node("Camera2D");
+var room:Vector2i;
 
-const SPEED = 80;
+const SPEED = 90;
 const JUMP_VELOCITY = -160
 const GRAVITY = 22
+const MAXFALLSPEED = 160;
 
-var coyotetime:float = 0;
-var coyotelimit:float = 2;
+var jumped:bool = false;
 
 var movingdirection:int = 0;
 var facingdirection:int = 1;
 
 func _physics_process(_delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += GRAVITY
-		coyotetime -= 1;
+	var onfloor:bool = is_on_floor();
+	
+	if onfloor:
+		jumped = false;
 	else:
-		coyotetime = coyotelimit;
-		
-	var onfloor:bool = (is_on_floor() or coyotetime >= 0);
+		velocity.y += GRAVITY
+		if velocity.y > MAXFALLSPEED:
+			velocity.y = MAXFALLSPEED;
 	
 	# Handle Jump.
 	if Input.is_action_just_pressed("confirm") and onfloor:
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		jumped = true;
+		onfloor = false;
+		movingdirection = facingdirection;
+	
 	var pressleft:bool = Input.is_action_pressed("left");
 	var pressright:bool = Input.is_action_pressed("right");
 	
-	if pressleft and pressright:
-		movingdirection = 0;
-	elif pressleft:
-		movingdirection = -1;
-		facingdirection = -1;
-	elif pressright:
-		movingdirection = 1;
-		facingdirection = 1;
+	if onfloor:
+		if pressleft and pressright:
+			movingdirection = 0;
+		elif pressleft:
+			movingdirection = -1;
+			facingdirection = -1;
+		elif pressright:
+			movingdirection = 1;
+			facingdirection = 1;
+		else:
+			movingdirection = 0;
 	else:
-		movingdirection = 0;
+		#If you didn't jump, fall straight down
+		if jumped:
+			movingdirection = facingdirection;
+		else:
+			movingdirection = 0;
 		
 	if movingdirection != 0:
 		velocity.x = movingdirection * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = 0
 		
 	#Animation
 	if !onfloor:
@@ -65,4 +75,24 @@ func _physics_process(_delta):
 			elif facingdirection > 0:
 				Sprite.play("right");
 		
-	move_and_slide()
+	move_and_slide();
+	updatecamera();
+
+func updatecamera():
+	var newroom:Vector2i = position;
+	@warning_ignore("integer_division")
+	newroom.x = int(floor((int(position.x) - (int(position.x) % 160)) / 160));
+	@warning_ignore("integer_division")
+	newroom.y = int(floor((int(position.y) - (int(position.y) % 90)) / 90));
+	
+	if(newroom != room):
+		room = newroom;
+		changeroom();
+	
+	camera.limit_left = int(room.x) * 160;
+	camera.limit_top = int(room.y) * 90;
+	camera.limit_right = camera.limit_left + 160;
+	camera.limit_bottom = camera.limit_top + 90;
+	
+func changeroom():
+	print("changed to room " + str(room))
