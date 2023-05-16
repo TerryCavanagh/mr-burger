8af658867e389632d254073ff59508799e94d15e
Terry Cavanagh <terrygcavanagh@gmail.com>
2023-05-16 14:57:03 +0100

    Place Coins and Dots from tilemap
    also updated beach stage1 to use this new feature

--- scripts/spawnfromtilemap.gd
+++ scripts/spawnfromtilemap.gd
@@ -5,13 +5,20 @@ extends Node2D
 const CLOUD:Array[Vector2i] = [Vector2i(4, 6)];
 const SEAWEED:Array[Vector2i] = [Vector2i(6, 3), Vector2i(6, 4), Vector2i(8, 4), Vector2i(8, 5)]
 
-var tilepositions:Array[Vector2] = [];
+const STAR:Array[Vector2i] = [Vector2i(1, 10)];
+const DOT:Array[Vector2i] = [Vector2i(2, 10)];
+const DOT_HIGH:Array[Vector2i] = [Vector2i(3, 10)];
+
+const BACKGROUND:Array[Vector2i] = [Vector2i(0, 0), Vector2i(5, 0), Vector2i(7, 0), Vector2i(2, 3), Vector2i(6, 6), Vector2i(9, 6), Vector2i(9, 7)];
 
 func _ready() -> void:
 	if tilemap != null:
-		tilepositions = getpositions(SEAWEED);
-		for t in tilepositions:
-			spawn(t, "killbox", "small");
+		spawnall(getpositions(SEAWEED), "killbox", ["small"]);
+		spawnall(getpositions(STAR), "coin", ["fixbackground"]);
+		spawnall(getpositions(DOT), "dot", ["fixbackground"]);
+		spawnall(getpositions(DOT_HIGH), "dot", ["fixbackground", "high"]);
+	
+	tilemap.force_update(0);
 
 func getpositions(tiletype:Array[Vector2i]) -> Array[Vector2]:
 	var completetilelist:Array[Vector2i] = [];
@@ -24,12 +31,36 @@ func getpositions(tiletype:Array[Vector2i]) -> Array[Vector2]:
 	
 	return tilelistfinal;
 
-func spawn(pos:Vector2, entitytype:String, variant:String = ""):
+func spawnall(positions:Array[Vector2], entitytype:String, variant:Array[String] = []):
+	for t in positions:
+		spawn(t, entitytype, variant);
+
+func spawn(pos:Vector2, entitytype:String, variant:Array[String] = []):
 	var newentity = World.entities[entitytype].instantiate();
 	newentity.position = pos;
 	
-	match variant:
-		"small":
-			newentity.get_node("Area2D").get_node("CollisionShape2D").shape.size = Vector2(6, 6);
+	for v in variant:
+		match v:
+			"small":
+				newentity.get_node("Area2D").get_node("CollisionShape2D").shape.size = Vector2(6, 6);
+			"high":
+				newentity.position.y -= 8;
+			"fixbackground":
+				@warning_ignore("narrowing_conversion")
+				var column:int = (pos.x / 16);
+				@warning_ignore("narrowing_conversion")
+				var row:int = (pos.y / 16)
+				var background:Vector2i = Vector2i(0, 0);
+				
+				var i:int = 0;
+				var tilemap_width:int = tilemap.get_used_rect().size.x;
+				while i < tilemap_width:
+					var tile = tilemap.get_cell_atlas_coords(0, Vector2i(i, row));
+					if BACKGROUND.has(tile):
+						background = tile;
+						i = tilemap_width;
+					i += 1;
+				
+				tilemap.set_cell(0, Vector2i(column, row), 0, background, 0);
 	
 	add_child(newentity);
