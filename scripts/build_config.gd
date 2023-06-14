198d7a38b4fba60f366937b933b6bfba1d488a1b
TerryCavanagh <terrygcavanagh@gmail.com>
2023-06-14 21:05:08 +0100

    fixing filename case issue

--- scripts/build_config.gd
+++ scripts/build_config.gd
@@ -4,5 +4,5 @@ const TESTLEVEL:bool = true;
 #const testlevelstage:String = "testlevel";
 #const testlevellevel:String = "testlevel";
 const testlevelstage:String = "dungeon";
-const testlevellevel:String = "stage1";
+const testlevellevel:String = "stage2";
 const LEVELSKIP:bool = true;
