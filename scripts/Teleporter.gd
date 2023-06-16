793a986ef827d3e7b7fca3aa1a73dd76f04aab03
TerryCavanagh <terrygcavanagh@gmail.com>
2023-06-16 21:16:43 +0100

    ok I slightly improved the teleporter thing

--- scripts/Teleporter.gd
+++ scripts/Teleporter.gd
@@ -1,8 +1,7 @@
 extends Area2D
-
-@export var movex:int = 0;
-@export var movey:int = 0;
-
+	
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		body.move_by(movex, movey);
+		var destination = get_node("destination");
+		var shape = get_node("CollisionShape2D");
+		body.move_by(0, destination.position.y - shape.position.y);
