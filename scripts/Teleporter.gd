8ecc287150c54861d83c2e1c25a622f6f5a23345
TerryCavanagh <terrygcavanagh@gmail.com>
2023-06-16 23:00:14 +0100

    working on a little stage 4 idea

--- scripts/Teleporter.gd
+++ scripts/Teleporter.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		var destination = get_node("destination");
 		var shape = get_node("CollisionShape2D");
-		body.move_by(0, destination.position.y - shape.position.y);
+		body.move_by(0, (destination.position.y - shape.position.y) + 16);
