2fcef2ec16fb14d71bbf4ef5b82bb484695913af
TerryCavanagh <terrygcavanagh@gmail.com>
2023-06-17 22:53:13 +0100

    New Beach stage!

--- scripts/build_config.gd
+++ scripts/build_config.gd
@@ -3,6 +3,6 @@ extends Node
 const TESTLEVEL:bool = true;
 #const testlevelstage:String = "testlevel";
 #const testlevellevel:String = "testlevel";
-const testlevelstage:String = "dungeon";
-const testlevellevel:String = "stage4";
+const testlevelstage:String = "beach";
+const testlevellevel:String = "stage2";
 const LEVELSKIP:bool = true;
