c052d7d1119a54f2efa6d286226d295b7fcc58f7
TerryCavanagh <terrygcavanagh@gmail.com>
2023-06-19 00:01:46 +0100

    Dungeon Stage 5

--- scripts/Teleporter.gd
+++ scripts/Teleporter.gd
@@ -1,7 +1,13 @@
 extends Area2D
+
+@export var movex:bool = false;
+@export var movey:bool = true;
 	
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		var destination = get_node("destination");
 		var shape = get_node("CollisionShape2D");
-		body.move_by(0, (destination.position.y - shape.position.y) + 16);
+		if movex:
+			body.move_by((destination.position.x - shape.position.x), (destination.position.y - shape.position.y) + 16);
+		else:
+			body.move_by(0, (destination.position.y - shape.position.y) + 16);
--- scripts/build_config.gd
+++ scripts/build_config.gd
@@ -3,6 +3,6 @@ extends Node
 const TESTLEVEL:bool = true;
 #const testlevelstage:String = "testlevel";
 #const testlevellevel:String = "testlevel";
-const testlevelstage:String = "beach";
-const testlevellevel:String = "stage3";
+const testlevelstage:String = "dungeon";
+const testlevellevel:String = "stage5";
 const LEVELSKIP:bool = true;
