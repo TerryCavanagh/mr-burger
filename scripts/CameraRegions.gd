96de94960052a5ac7821b33affe82c6d8f73490d
Terry Cavanagh <terrygcavanagh@gmail.com>
2023-07-11 15:08:37 +0100

    Added collision layers and masks to fix some collision issues
    This will probably cause a few more, but should be easily fixed one by one

--- scripts/CameraRegions.gd
+++ scripts/CameraRegions.gd
@@ -1,4 +1,8 @@
-extends Node2D
+extends Area2D
+
+func _ready():
+	set_collision_mask_value(1, false)
+	set_collision_mask_value(2, true)
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
