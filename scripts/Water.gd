265118e39d5744ba9edb02c049e258cde2ed5c9b
Terry Cavanagh <terrygcavanagh@gmail.com>
2023-07-11 15:17:27 +0100

    Fixed a couple more collision mask issues

--- scripts/Water.gd
+++ scripts/Water.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+func _ready():
+	set_collision_mask_value(1, false)
+	set_collision_mask_value(2, true)
+	
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.addwaterzone(self);
