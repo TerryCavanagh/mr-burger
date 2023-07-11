a999c4abca18e007c750f991b4f142a21e61b7cb
Terry Cavanagh <terrygcavanagh@gmail.com>
2023-07-11 16:24:09 +0100

    New forest level

--- scripts/build_config.gd
+++ scripts/build_config.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const TESTLEVEL:bool = true;
-const testlevelstage:String = "testlevel";
-const testlevellevel:String = "testlevel";
-#const testlevelstage:String = "burger";
-#const testlevellevel:String = "stage1";
+#const testlevelstage:String = "testlevel";
+#const testlevellevel:String = "testlevel";
+const testlevelstage:String = "forest";
+const testlevellevel:String = "stage2";
 const LEVELSKIP:bool = true;
